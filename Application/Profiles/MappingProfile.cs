87a409eca7901a6da6cbc2a48d722602ec84cd41
fasika <fasikafikadu38@gmail.com>
2023-10-13 14:23:18 +0300

    add profiles

---
+++ Application/Profiles/MappingProfile.cs
@@ -0,0 +1,21 @@
+using AutoMapper;
+using Application.DTOs.Movie;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Application.DTOs.Cinema;
+
+namespace MovieAPI.Application.Profiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Movie, CreateMovieDTO>().ReverseMap();
+            CreateMap<Movie, UpdateMovieDTO>().ReverseMap();
+            CreateMap<Cinema, CreateCinemaDTO>().ReverseMap();
+            CreateMap<Cinema, UpdateCinemaDTO>().ReverseMap();
+        }
+    }
+}
